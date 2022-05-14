244b584e6818d2cfcc34a7da5cf198a44ebac821
LoopFruits <Kensworldc@gmail.com>
2022-05-14 17:44:33 -0400

    completed assignment:

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,28 @@
 // Code EyesOnMe Component Here
+import React from "react";
+
+
+
+
+function EyesOnMe(){
+
+    function HandleFocus(){
+        console.log('Good!');
+    }
+
+    function HandleBlur(){
+        console.log('Hey! Eyes on me!')
+    }
+
+    return(
+        <button onFocus={HandleFocus} onBlur={HandleBlur}>
+            Eyes on me
+        </button>
+
+        
+    );
+}
+
+
+
+export default EyesOnMe;
